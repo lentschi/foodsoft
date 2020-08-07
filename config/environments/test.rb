48b4a24787b41ce6962d58be40aa34cbee08be8c
Patrick Gansterer <paroga@paroga.com>
2020-08-07 11:52:13 +0200

    Add support for ActionCable

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
+
+  config.action_cable.mount_path = '/:foodcoop/cable'
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
